c8fbc4ac6e5c2aed019801ba34ac6424336000d0
ganeshbodakhe24 <156433243+ganeshbodakhe24@users.noreply.github.com>
2026-02-13 01:14:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded",function(){
-let form =document.getElementById("form");
+document.addEventListener("DOMContentLoaded", function(){
 
-function addData(data){
-	let tbody=document.getElementById("book-list");
-	let tr=document.createElement("tr");
-	let td1=document.createElement("td");
-	let td2=document.createElement("td");
-	let td3=document.createElement("td");
-	let td4=document.createElement("td");
-	let button=document.createElement("button");
-	td1.textContent=data.title;
-	td2.textContent=data.author;
-	td3.textContent=data.isbn;
-	button.textContent="delete";
-	td4.classList="delete";
-	td4.append(button)
-	tr.append(td1,td2,td3,td4);
-	tbody.append(tr);
-}
-form.addEventListener("submit", function (event){
-	event.preventDefault();
-	let formData=new FormData(event.target);
-	let title=formData.get("title");
-	let author=formData.get("author");
-	let isbn=formData.get("isbn");
-	let data={title,author,isbn};
-	addData(data);
-})
+	let form = document.getElementById("form");
 
-document.getElementById("book-list").addEventListener("click",function(event){
-	if(event.target.tagName="button"){
-		if(event.target.parentNode.classList.contains("delete")){
-			event.target.parentNode.parentNode.remove()
-		}
+	function addData(data){
+		let tbody = document.getElementById("book-list");
+
+		let tr = document.createElement("tr");
+
+		let td1 = document.createElement("td");
+		let td2 = document.createElement("td");
+		let td3 = document.createElement("td");
+		let td4 = document.createElement("td");
+
+		let button = document.createElement("button");
+
+		td1.textContent = data.title;
+		td2.textContent = data.author;
+		td3.textContent = data.isbn;
+
+		button.textContent = "Delete";
+		button.classList.add("delete");
+
+		td4.append(button);
+		tr.append(td1, td2, td3, td4);
+		tbody.append(tr);
 	}
-})
-let deleteButtons = document.querySelectorAll(".delete");
 
-for (let i = 0; i < deleteButtons.length; i++) {
-	deleteButtons[i].addEventListener("click", function() {
-		this.parentNode.parentNode.remove();
+	form.addEventListener("submit", function(event){
+		event.preventDefault();
+
+		let formData = new FormData(event.target);
+
+		let data = {
+			title: formData.get("title"),
+			author: formData.get("author"),
+			isbn: formData.get("isbn")
+		};
+
+		addData(data);
 	});
-}
-	
-})
+
+	document.getElementById("book-list").addEventListener("click", function(event){
+
+		if (event.target.classList.contains("delete")) {
+			event.target.closest("tr").remove();
+		}
+
+	});
+
+});
